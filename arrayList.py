c6778f0eca91ec1231d6bca29a2430f7c6fc7b44
Davit <your.email@example.com>
2024-02-26 11:09:55 +0400

    learn list all method

---
+++ arrayList.py
@@ -0,0 +1,70 @@
+# names = ["john","smith","Julie"]
+# ages = [1,2,3]
+# mixedArray = ["Arman",2,3,True,"Gevorg"]
+# mixedArray[0] = "Davit"
+# lastElement = mixedArray[-1]
+
+# mixedArray.append("Ben")
+# mixedArray.pop()
+
+# mixedList2 = [
+#     ["Gevor","Goqor","Artash"],
+#     [1,2,3,4,5,6]
+# ]
+
+# totalList = mixedList2[0] + mixedList2[-1] # concat lists
+# multipleList = mixedList2 * 3 # concat lists in multiple time
+# multipleListItemOne = mixedList2[0][0] * 3 # concat lists in multiple time
+# newmixedArray = mixedArray[2:4]
+
+# array = array = [1, 2, 3, 4, 5]
+# array.remove(1)
+# del array[0]
+# array.pop(1)
+# array.append(6)
+# array.extend([7, 8])
+# array.insert(8, 100)
+# print(array)
+
+my_list = [1, 2, 3]
+
+# # append
+# my_list.append(4)
+
+# # extend
+# my_list.extend([5, 6])
+
+# insert
+# my_list.insert(0, 0)
+
+# remove 
+# my_list.remove(2)
+
+# # pop
+# popped_element = my_list.pop()
+
+# # index
+# index_of_2 = my_list.index(2)
+
+# # element count in array 
+# count_of_1 = my_list.count(1)
+
+# # sort
+# my_list.sort()
+
+# # reverse
+# my_list.reverse()
+
+# # copy
+# copied_list = my_list.copy()
+
+# length = len(my_list)
+
+# get memory id name
+# memory_address = id(my_list)
+
+# get memory hexademical name
+# hex_memory_address = hex(memory_address)
+
+
+# print(hex_memory_address)
