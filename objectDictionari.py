15de32757432897061d5cb8ae51656a91f160bcc
Davit <your.email@example.com>
2024-02-27 10:43:42 +0400

    change order

--- objectDictionari.py
+++ objectDictionari.py
@@ -1,19 +1,19 @@
-product = {
-    "boys" : [1,2,3],
-    "men" : {"name":"john"},
-}
+# product = {
+#     "boys" : [1,2,3],
+#     "men" : {"name":"john"},
+# }
 
-var_type = type(product)
-var_producttype = type(product.get("sss"))
-product["boys"].extend([4,5])
+# var_type = type(product)
+# var_producttype = type(product.get("sss"))
+# product["boys"].extend([4,5])
 
-data = {
-    "men" : {"name":"john"},
-}
+# data = {
+#     "men" : {"name":"john"},
+#     "men": {"name":"sdsd"}
+# }
 
-data.update({"name":"dddd"})
-
-keys = list(data.keys())
-values = data.values()
-
-print(keys[0])
+# name =  "vardan"
+# fString = f"name + {name}"  
+# data.update({"name":"dddd"})
+# keys = list(data.keys())
+# values = data.values()
---
+++ tuply.py
@@ -0,0 +1,6 @@
+# it is a same as list , but you can n ot chnage or add , it fast than list
+
+name = ("john","smith","Davit")
+mixedTuple = 11,22,33
+typeNAme = type(name)
+print(mixedTuple[:-1])
