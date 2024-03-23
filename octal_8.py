3fd3eea9222f42c3f0bd8a924c651dc3b749487b
Davit <torgomyandavid96@gmail.com>
2024-03-23 11:26:42 +0400

    master

---
+++ octal_8.py
@@ -0,0 +1,15 @@
+# Octal 755 is equal to decimal 493
+# Decimal 493 is equal to binary 111101101
+
+# BASE 8 => Octal
+# as a octal    4   2   1    represent 3 bit 
+# as a binary   1   0   0    => equalt to plus all => 4  as a octal 
+# as a binary   1   0   1    => equalt to plus all => 5  as a octal 
+# as a binary   1   1   1    => equalt to plus all => 7  as a octal 
+# 100   101  111 = > 457
+# = V
+
+# octal 457 => (7 * 8 ** 0) + (5 * 8 ** 1) + (4 * 8 ** 2) => decimal 303
+# print((7 * 8 ** 0) + (5 * 8 ** 1) + (4 * 8 ** 2))
+
+
