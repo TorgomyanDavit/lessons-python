618e69d6ff617fe31c3631d45c4fd7f72fc335fd
Davit <your.email@example.com>
2024-06-06 17:28:13 +0400

    add images logic in nataliWeb

---
+++ refeatLessons/dataType.py
@@ -0,0 +1,32 @@
+
+'''
+print('hello world '+"from arman")
+'''
+# print('hello world '+"from arman")
+
+# print('2024' + '15')
+# print(10 * 5)
+# print("10" / "5")
+
+'''
+country = ' Armenia'
+print('hello' + country)
+print('hello' + country)
+print('hello' + country)
+print('hello' + country)
+'''
+
+# x = "15"
+# y = "20"
+# print(x + y)
+
+
+# boolOne = True
+# bollTwo = False
+# print(boolOne)
+# print(bollTwo)
+
+
+# x = "15"
+# y = "20"
+# print(x < y)
