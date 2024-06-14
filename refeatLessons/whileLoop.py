8ec01becf86c19148c7e263de28633d5e6954378
Davit <your.email@example.com>
2024-06-14 17:46:41 +0400

    add header to product list

---
+++ refeatLessons/whileLoop.py
@@ -0,0 +1,27 @@
+
+
+
+# count = 0
+# while count < 10  :
+#     if(count == 5):break
+#     print(count)
+#     count += 1
+
+# count = 0
+# while count < 10  :
+#     count += 1
+#     if(count == 5):
+#         continue
+#     print(count)
+
+
+while True:
+    print(1)
+    comand = input()
+
+    if(int(comand) == 8):
+        print("exit")
+        break
+    
+
+
