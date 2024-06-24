e91dbf65d736df5a2cf88c660ca85d4aabd6f8fe
Davit <torgomyandavid96@gmail.com>
2024-06-24 21:01:08 +0400

    oop

---
+++ refeatLessons/oop/oop.py
@@ -0,0 +1,31 @@
+
+# class Profile:
+#     pass
+
+# user1 = Profile()
+# user1.first_name = "Davit"
+# user1.last_name = "Torgomyan"
+# user1.profile_picture = "Davit.jpeg"
+# user1.cover_picture = "Davit_cover.jpeg"
+
+# user2 = Profile()
+# user2.first_name = "Arman"
+# user2.last_name = "Avetisyan"
+# user2.profile_picture = "Arman.jpeg"
+# user2.cover_picture = "Arman_cover.jpeg"
+
+
+
+class Profile:
+    def __init__(self,first_name,last_name,profile_picture,cover_picture):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.profile_picture = profile_picture
+        self.cover_picture = cover_picture
+
+
+user1 = Profile("Davit","Torgomyan","Davit.jpeg","Davit_cover.jpeg")
+user2 = Profile("Arman","Avetisyan","Arman.jpeg","Arman_cover.jpeg")
+
+print(f"{user1.first_name} {user1.last_name}")
+print("{} {}".format(user2.first_name,user2.last_name))
