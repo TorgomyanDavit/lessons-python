274ef8c39aeee5d54201e57b8881279127c2d365
Davit <torgomyandavid96@gmail.com>
2024-07-17 22:40:46 +0400

    master

--- ifElse.py
+++ ifElse.py
@@ -1,16 +1,16 @@
-print("Write number")
+# print("Write number")
 
 
-comand = input()
-login = True
+# comand = input()
+# login = True
 
-if (comand == 1): 
-    print("open home page")
-elif (comand == 2 and login == True) : 
-    print("Open settings page")
-elif (comand == 3 and login == True) : 
-    print("Open settings page")
-elif (comand == 4 or login == True) and 5 < 6 : 
-    print("Open Profile page")
-else : 
-    print("Exit Aplication")
+# if (comand == 1): 
+#     print("open home page")
+# elif (comand == 2 and login == True) : 
+#     print("Open settings page")
+# elif (comand == 3 and login == True) : 
+#     print("Open settings page")
+# elif (comand == 4 or login == True) and 5 < 6 : 
+#     print("Open Profile page")
+# else : 
+#     print("Exit Aplication")
